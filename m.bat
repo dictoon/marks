52a35ab2414d58221d1874fcbf4e53c0cb790422
Jeremy Murray <jeremy.murray@gmail.com>
2013-08-18 19:35:51 -0700

    Prevent odd mark names.

--- m.bat
+++ m.bat
@@ -1,5 +1,6 @@
 @echo off
 if ("%~1") == ("") echo Please specify mark name.&&exit /b 1
+for /f "delims=*+,/:;<=>?\[]|" %%a in ("%~1") do if not ("%%~a") == ("%~1") echo Please do not use special characters in the mark name.&&exit /b 1
 if not exist "%~dp0marks" mkdir "%~dp0marks"
 echo %CD%>"%~dp0marks\%~1.mark"
 
