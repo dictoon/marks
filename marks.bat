68f40c5cafda58dd295285730ee6186e2066d643
Jeremy Murray <jeremy.murray@gmail.com>
2013-08-18 19:36:18 -0700

    Change arrow to match original.

--- marks.bat
+++ marks.bat
@@ -1,4 +1,4 @@
 @echo off
 if not exist "%~dp0marks\*.mark" echo No marks exist.&&exit /b 1
-for /f "delims=" %%f in ('dir /b "%~dp0marks\*.mark"') do (for /F "usebackq delims=" %%i in ("%~dp0marks\%%f") do echo %%~nf =^> %%i)
+for /f "delims=" %%f in ('dir /b "%~dp0marks\*.mark"') do (for /F "usebackq delims=" %%i in ("%~dp0marks\%%f") do echo %%~nf -^> %%i)
 
